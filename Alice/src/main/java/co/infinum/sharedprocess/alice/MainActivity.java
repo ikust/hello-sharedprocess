73dbe51a3572adcc0bee988d71b57be62173637b
Ivan Kušt <ivan@infinum.hr>
2014-02-05 10:38:08 +0100

    - cleaned up the examples a bit  - added comments

--- Alice/src/main/java/co/infinum/sharedprocess/alice/MainActivity.java
+++ Alice/src/main/java/co/infinum/sharedprocess/alice/MainActivity.java
@@ -5,40 +5,77 @@ import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.content.res.Resources;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
 import android.support.v7.app.ActionBarActivity;
-import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
-import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 
+/**
+ * An Activity that demonstrates starting Activity from another application sharing the same
+ * sharedUserId and passing data to it.
+ * <p>
+ * A prerequisite for this is that sharedUserId must be set in AndroidManifests of both applications
+ * to the same value, and both applications must be signed with the same keystore.
+ */
 public class MainActivity extends ActionBarActivity {
 
+    /**
+     * Fully qualified name of Bob's activity that will be started.
+     */
     private static final String BOBS_MAIN_ACTIIVTY = "co.infinum.sharedprocess.bob.MainActivity";
+
+    /**
+     * Package identifier of Bob's application.
+     */
+    private static final String BOBS_PACKAGE = "co.infinum.sharedprocess.bob";
+
+    /**
+     * Key of the extra that will be passed to Bob's activity.
+     */
+    private static final String EXTRA_SECRET_MESSAGE = "message";
+
+    /**
+     * Edit text for entering message to send.
+     */
+    private EditText messageEditText;
+
+    /**
+     * Send button.
+     */
+    private Button sendButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        if (savedInstanceState == null) {
-            getSupportFragmentManager().beginTransaction()
-                    .add(R.id.container, new PlaceholderFragment())
-                    .commit();
-        }
-        tellSecretToBob("Shhh this is a secret");
+        final EditText messageEditText = (EditText) findViewById(R.id.editTextMessage);
+        Button sendButton = (Button) findViewById(R.id.buttonSend);
+
+        sendButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                tellSecretToBob(messageEditText.getText().toString());
+            }
+        });
     }
 
+    /**
+     * Returns a resource identifier for a given resource name and type.
+     *
+     * @param resourceName name of the resource
+     * @param resourceType type of the resource (e.g. drawable, string, layout...)
+     * @return resource identifier or 0 if there is no such resource
+     */
     public int getBobsResource(String resourceName, String resourceType) {
         PackageManager pm = getPackageManager();
 
         Resources resources = null;
         if (pm != null) {
             try {
-                resources = pm.getResourcesForApplication("co.infinum.sharedprocess.bob");
+                resources = pm.getResourcesForApplication(BOBS_PACKAGE);
 
-                int res = resources.getIdentifier(resourceName, resourceType, "co.infinum.sharedprocess.bob");
+                int res = resources.getIdentifier(resourceName, resourceType, BOBS_PACKAGE);
             } catch (PackageManager.NameNotFoundException e) {
                 e.printStackTrace();
             }
@@ -47,44 +84,20 @@ public class MainActivity extends ActionBarActivity {
         return 0;
     }
 
+    /**
+     * Starts Bob's activity defined with fully qualified class name in BOBS_MAIN_ACTIVITY constant.
+     * Bob's application package name is defined in BOBS_PACKAGE. A String extra with the key
+     * EXTRA_SECRET_MESSAGE is passed, containing the message given in argument.
+     *
+     * @param message message to pass to Bob's activity
+     */
     public void tellSecretToBob(String message) {
         Intent intent = new Intent();
 
-        intent.setComponent(new ComponentName("co.infinum.sharedprocess.bob", "co.infinum.sharedprocess.bob.MainActivity"));
-        intent.putExtra("secret", "secret message");
+        intent.setComponent(new ComponentName(BOBS_PACKAGE, BOBS_MAIN_ACTIIVTY));
+        intent.putExtra(EXTRA_SECRET_MESSAGE, message);
         startActivity(intent);
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
-
-    /**
-     * A placeholder fragment containing a simple view.
-     */
-    public static class PlaceholderFragment extends Fragment {
-
-        public PlaceholderFragment() {
-        }
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                                 Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
-            return rootView;
-        }
-    }
 
 }
--- Bob/src/main/java/co/infinum/sharedprocess/bob/MainActivity.java
+++ Bob/src/main/java/co/infinum/sharedprocess/bob/MainActivity.java
@@ -1,80 +1,52 @@
 package co.infinum.sharedprocess.bob;
 
 import android.app.AlertDialog;
-import android.content.BroadcastReceiver;
-import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
 import android.support.v7.app.ActionBarActivity;
-import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
 
+/**
+ * An activity that receives a message over intent.
+ * <p/>
+ * Message is read from a string extra passed in intent with the key defined in
+ * EXTRA_SECRET_MESSAGE_CONSTNT.
+ */
 public class MainActivity extends ActionBarActivity {
 
-    private BroadcastReceiver receiver = new BroadcastReceiver() {
-
-        @Override
-        public void onReceive(Context context, Intent intent) {
-
-        }
-    };
+    /**
+     * Key of the extra that will be passed to Bob's activity.
+     */
+    private static final String EXTRA_SECRET_MESSAGE = "message";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
 
-        if (savedInstanceState == null) {
-            getSupportFragmentManager().beginTransaction()
-                    .add(R.id.container, new PlaceholderFragment())
-                    .commit();
-        }
+        displayMessage(getIntent());
+    }
 
-        Bundle extras = getIntent().getExtras();
+    @Override
+    protected void onNewIntent(Intent intent) {
+        super.onNewIntent(intent);
 
-        if(extras != null && extras.containsKey("secret")) {
+        displayMessage(intent);
+    }
+
+    /**
+     * Displays the received message in a dialog.
+     */
+    private void displayMessage(Intent intent) {
+        Bundle extras = intent.getExtras();
+
+        if (extras != null && extras.containsKey(EXTRA_SECRET_MESSAGE)) {
             AlertDialog.Builder builder = new AlertDialog.Builder(this);
-            builder.setTitle("Message from alice");
-            builder.setMessage(extras.getString("secret"));
+            builder.setTitle("Message from Alice");
+            builder.setMessage(extras.getString(EXTRA_SECRET_MESSAGE));
             builder.setPositiveButton("Ok", null);
             builder.show();
         }
     }
 
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
-
-    /**
-     * A placeholder fragment containing a simple view.
-     */
-    public static class PlaceholderFragment extends Fragment {
-
-        public PlaceholderFragment() {
-        }
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_main2, container, false);
-            return rootView;
-        }
-    }
 
 }
